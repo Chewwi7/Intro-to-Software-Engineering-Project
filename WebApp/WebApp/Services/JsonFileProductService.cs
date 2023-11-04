9296cfe3b1bcfe2db06f4978b14a1d06f6692a35
PNsajja <pnsajja@gmail.com>
2023-11-04 18:59:11 -0400

    Added JsonFileProductServices

---
+++ WebApp/WebApp/Services/JsonFileProductService.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
